5430a39be476463908c69e79a685222c4fecbc95
rajeshm7 <rajeshm916@gmail.com>
2021-05-17 20:51:51 +0530

    Set authorize using preAuthorise annotations

--- src/main/java/com/raj/security/student/StudentManagementController.java
+++ src/main/java/com/raj/security/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.raj.security.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -16,19 +17,23 @@ public class StudentManagementController {
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents(){
         return students;
     }
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student){
         System.out.println("registerNewStudent");
         System.out.println(student);
     }
     @DeleteMapping("/{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId){
         System.out.println(studentId);
     }
     @PutMapping(path="/{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student){
         System.out.println(String.format("%s %s", studentId, student));
     }
