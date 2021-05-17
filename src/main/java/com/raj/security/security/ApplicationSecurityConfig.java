3b2809dfa2c7127fdc4b4a9c9c27d52f12c888e4
rajeshm7 <rajeshm916@gmail.com>
2021-05-17 23:15:14 +0530

    Changed from httpbasic to formbased

--- src/main/java/com/raj/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/raj/security/security/ApplicationSecurityConfig.java
@@ -46,7 +46,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin();
     }
 
     @Override
