1cbf871bdccb7e65359558217b1a7022f890db13
rajeshm7 <rajeshm916@gmail.com>
2021-05-18 18:47:19 +0530

    ApplcationUserDaoImpl

---
+++ src/main/java/com/raj/security/auth/FakeApplicationUserDaoService.java
@@ -0,0 +1,63 @@
+package com.raj.security.auth;
+
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.raj.security.security.ApplicationUserRole.STUDENT;
+
+@Repository("Fake")
+public class FakeApplicationUserDaoService implements  ApplicationUserDao{
+
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public FakeApplicationUserDaoService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Optional<ApplicationUser> selectApplicationUserByUsername(String username) {
+        return getApplicationUser().stream()
+                .filter(applicationUser -> applicationUser.getUsername().equals(username))
+                .findFirst();
+    }
+
+    private List<ApplicationUser> getApplicationUser(){
+        List<ApplicationUser> applicationUsers = Lists.newArrayList(
+                new ApplicationUser(
+                        STUDENT.getGrantedAuthorities(),
+                     "student" ,
+                     "password",
+                     true,
+                     true,
+                     true,
+                     true
+                ),
+                new ApplicationUser(
+                        STUDENT.getGrantedAuthorities(),
+                        "admin" ,
+                        "password",
+                        true,
+                        true,
+                        true,
+                        true
+                ),
+                new ApplicationUser(
+                        STUDENT.getGrantedAuthorities(),
+                        "adminTrainee" ,
+                        "password",
+                        true,
+                        true,
+                        true,
+                        true
+                )
+        );
+        return applicationUsers;
+    }
+
+}
