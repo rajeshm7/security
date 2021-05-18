106d386b949911ea978b9e0ad547aba6da3aa4e1
rajeshm7 <rajeshm916@gmail.com>
2021-05-18 18:15:27 +0530

    ApplcationUserDao interface

---
+++ src/main/java/com/raj/security/auth/ApplicationUserDao.java
@@ -0,0 +1,9 @@
+package com.raj.security.auth;
+
+import java.util.Optional;
+
+public interface ApplicationUserDao {
+
+    public Optional<ApplicationUser> selectApplicationUserByUsername(String username);
+
+}
--- src/main/java/com/raj/security/auth/ApplicationUserService.java
+++ src/main/java/com/raj/security/auth/ApplicationUserService.java
@@ -7,9 +7,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ApplicationUserService implements UserDetailsService {
-    
+
+    private ApplicationUserDao applicationUserDao;
+
+    public ApplicationUserService(ApplicationUserDao applicationUserDao) {
+        this.applicationUserDao = applicationUserDao;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return applicationUserDao.selectApplicationUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("Username %s not found",username)));
     }
 }
